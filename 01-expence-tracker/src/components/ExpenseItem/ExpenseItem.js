b816a68324c2d8e8abef677d9f4a81ca22d2f2f2
dilberhusainkhan <dilberjmi@gmail.com>
2023-03-13 23:23:21 +0530

    Update Event Handeling

--- 01-expence-tracker/src/components/ExpenseItem/ExpenseItem.js
+++ 01-expence-tracker/src/components/ExpenseItem/ExpenseItem.js
@@ -2,15 +2,23 @@ import React from 'react'
 import ExpenseDate from '../ExpenseDate/ExpenseDate'
 import './ExpenseItem.css'
 import Card from '../Card/Card'; 
+
+
 const ExpenseItem = (data) => {
-  
+
+  let title = data.title;
+  const clickHandler=()=>{
+    console.log("Update title");
+    title="Update";
+  }
   return (
     <Card className='expense-item'>
         <ExpenseDate date = {data.date}/>
         <div className='expense-item__description'>
-            <h2>{data.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${data.price}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
